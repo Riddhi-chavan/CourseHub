4a47279b30e03b17d6fd4c263e97ab8297558bf4
Riddhi-chavan <riddhic164@gmail.com>
2024-06-13 00:16:28 +0530

    Added sign in  design

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -21,67 +21,65 @@ const Signin = () => {
   };
 
   return (
-    <div class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-700">
-      <button
-        class="flex items-center bg-white dark:bg-gray-900 border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        onClick={handleGoogle}
-      >
-        <svg
-          class="h-6 w-6 mr-2"
-          width="800px"
-          height="800px"
-          viewBox="-0.5 0 48 48"
-          version="1.1"
-        >
-          {" "}
-          <title>Google-color</title>
-          <defs> </defs>{" "}
-          <g
-            id="Icons"
-            stroke="none"
-            stroke-width="1"
-            fill="none"
-            fill-rule="evenodd"
-          >
-            {" "}
-            <g id="Color-" transform="translate(-401.000000, -860.000000)">
-              {" "}
-              <g id="Google" transform="translate(401.000000, 860.000000)">
-                {" "}
-                <path
-                  d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-                  id="Fill-1"
-                  fill="#FBBC05"
+    <div className="h-screen w-screen bg-gray-400">
+      <div className="fixed grid place-items-center backdrop-blur-sm top-0 right-0 left-0 z-50 w-full inset-0 h-modal h-full justify-center items-center">
+        <div className="relative container m-auto px-6 md:w-[25rem]">
+          <div className="m-auto">
+            <div className="rounded-xl bg-white dark:bg-gray-800 shadow-xl">
+              <div className="p-8 ">
+                <button
+                  type="button"
+                  className="absolute top-2 right-8 p-2 text-cyan-900 dark:text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm ml-auto inline-flex items-center popup-close"
                 >
-                  {" "}
-                </path>{" "}
-                <path
-                  d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-                  id="Fill-2"
-                  fill="#EB4335"
-                >
-                  {" "}
-                </path>{" "}
-                <path
-                  d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-                  id="Fill-3"
-                  fill="#34A853"
-                >
-                  {" "}
-                </path>{" "}
-                <path
-                  d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-                  id="Fill-4"
-                  fill="#4285F4"
-                >
-                  {" "}
-                </path>{" "}
-              </g>{" "}
-            </g>{" "}
-          </g>{" "}
-        </svg>
-        <span>Continue with Google</span>
-      </button>
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="#c6c7c7"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      cliprule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Close popup</span>
+                </button>
+                <div className="space-y-4">
+                  <img src="/dashboard.png" loading="lazy" className="w-10" />
+                  <h2 className="mb-8 text-2xl text-cyan-900 dark:text-white font-bold">
+                    Sign-in to unlock the <br />
+                    best of CourseHub.
+                  </h2>
+                </div>
+                <div className="mt-10 grid space-y-4">
+                  <button
+                    className="group h-12 px-2 md:px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
+                    onClick={handleGoogle}
+                  >
+                    <div className="relative flex items-center space-x-4 justify-center">
+                      <img
+                        src="https://www.svgrepo.com/show/475656/google-color.svg"
+                        className="absolute left-0 w-5 "
+                        alt="google logo"
+                      />
+                      <span className="block w-max font-semibold tracking-wide text-gray-700 dark:text-white text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
+                        Continue with Google
+                      </span>
+                    </div>
+                  </button>
+                </div>
+                <div className="mt-14 space-y-4 py-3 text-gray-600 dark:text-gray-400 text-center">
+                  <p className="text-xs">
+                    By proceeding, You will be sign-in in to our CourseHub
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
